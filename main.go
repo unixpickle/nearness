d77d9f7da70e1553febaeb8bcab02dd90f47090c
Alex Nichol <unixpickle@gmail.com>
2019-08-01 19:04:33 -0400

    try solving all the boards at once

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"log"
+	"strings"
+)
 
 func main() {
-	board := NewBoard(30).Shuffle()
-	for {
-		l1 := board.NormNearness()
-		fmt.Printf("loss=%f\n", l1)
-		b1 := board.Copy()
-		b1.Mutate()
-		l2 := b1.NormNearness()
-		if l2 < l1 {
-			board = b1
+	solutions := map[int]*Board{}
+	for i := 6; i <= 30; i++ {
+		solutions[i] = NewBoard(i).Shuffle()
+	}
+	for step := 0; step < 10000000; step++ {
+		for size, b := range solutions {
+			b1 := b.Copy()
+			b1.Mutate()
+			if b1.Nearness() < b.Nearness() {
+				solutions[size] = b1
+			}
+		}
+		if step%10000 == 0 {
+			log.Println("saving solution set")
+			SaveSolutions(solutions)
 		}
 	}
+}
+
+func SaveSolutions(solutions map[int]*Board) {
+	strs := make([]string, 0, len(solutions))
+	for _, b := range solutions {
+		strs = append(strs, b.String())
+	}
+	data := []byte(strings.Join(strs, ";\n"))
+	ioutil.WriteFile("solutions.txt", data, 0755)
 }
